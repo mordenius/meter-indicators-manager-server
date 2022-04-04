26f070095886200f6042dea1a8ebe3344f133c19
mordenius <mordenius@gmail.com>
2022-04-04 04:28:56 +0300

    spec(meter/changes) service

---
+++ src/meters/changes/__tests__/meterChange.service.spec.ts
@@ -0,0 +1,43 @@
+import dotenv from "dotenv";
+import { DataSource, DeepPartial } from "typeorm";
+
+import { Meter } from "../../meter.entity";
+import { MeterChange } from "../meterChange.entity";
+import { MeterChangeService } from "../meterChange.service";
+
+import {
+  initializeLogger,
+  initializeDataSource
+} from "../../../_frameworks/index";
+
+let dataSource: DataSource;
+
+beforeAll(
+  async (): Promise<void> => {
+    dotenv.config();
+    const logger = initializeLogger();
+    dataSource = await initializeDataSource(logger, [Meter, MeterChange]);
+  }
+);
+
+afterAll(
+  async (): Promise<void> => {
+    await dataSource.destroy();
+  }
+);
+
+describe("Meter Service", (): void => {
+  it("Create a new meter", async (): Promise<void> => {
+    const meterChangeService = new MeterChangeService(dataSource);
+
+    const data: DeepPartial<MeterChange> = {
+      previousValue: 0,
+      currentValue: 1
+    };
+
+    const change = await meterChangeService.create(data);
+
+    expect(change.previousValue).toBe(data.previousValue);
+    expect(change.currentValue).toBe(data.currentValue);
+  });
+});
