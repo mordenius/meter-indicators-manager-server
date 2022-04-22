fd60937c0d2a083d6f13eb36aa8e04a39f8b4b1e
mordenius <mordenius@gmail.com>
2022-04-22 04:33:48 +0300

    spec(auth/users) user entity

---
+++ src/auth/users/__tests__/user.entity.spec.ts
@@ -0,0 +1,67 @@
+import { User, UserInDto } from "./../user.entity";
+
+import { startApp, stopApp, dropCollection } from "./__helpers__";
+
+beforeAll(
+  async (): Promise<void> => {
+    await startApp();
+    await dropCollection(User);
+  }
+);
+
+afterAll(
+  async (): Promise<void> => {
+    await dropCollection(User);
+    await stopApp();
+  }
+);
+
+const TEST_USER_EMAIL = "test@test.com";
+
+describe("User entity", (): void => {
+  it(`should the collection be empty
+   before testing`, async (): Promise<void> => {
+    const sessions = await User.find();
+    expect(sessions.length).toEqual(0);
+  });
+
+  it(`should create a new user`, async (): Promise<void> => {
+    const data: UserInDto = {
+      email: TEST_USER_EMAIL,
+      pswd: "qwerty"
+    };
+
+    const user = await User.create(data);
+
+    expect(user.email).toBe(data.email);
+    expect(user.pswd).toBe(data.pswd);
+  });
+
+  it(`should hide password while find a user`, async (): Promise<void> => {
+    const user = await User.findOne({ email: TEST_USER_EMAIL });
+
+    if (!user) {
+      expect(true).toBeFalsy();
+      return;
+    }
+
+    expect(user.email).toBe(TEST_USER_EMAIL);
+    expect(user.pswd).toBeUndefined();
+  });
+
+  it(`should create timestamps for a document`, async (): Promise<void> => {
+    const user = await User.findOne({ email: TEST_USER_EMAIL });
+
+    if (!user) {
+      expect(true).toBeFalsy();
+      return;
+    }
+
+    expect(Number.isNaN(new Date("invalid").getTime())).toBeTruthy();
+
+    expect(user.createdAt).not.toBeUndefined();
+    expect(Number.isNaN(new Date(user.createdAt).getTime())).toBeFalsy();
+    expect(user.updatedAt).not.toBeUndefined();
+    expect(Number.isNaN(new Date(user.updatedAt).getTime())).toBeFalsy();
+  });
+});
