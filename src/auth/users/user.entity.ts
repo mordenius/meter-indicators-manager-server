38167eeaade26c33f1da3bddcdd0289ee1f73da5
mordenius <mordenius@gmail.com>
2022-04-22 04:34:05 +0300

    feature(auth/users) user entity

---
+++ src/auth/users/user.entity.ts
@@ -0,0 +1,36 @@
+import { Schema, Model, model, ObjectId, Document } from "mongoose";
+
+import { ReqisterTool } from "./../_frameworks";
+
+export const COLLECTION_NAME = "User";
+export const USER_MODEL_NAME = "USER_MODEL";
+
+export interface UserInDto {
+  email: string;
+  pswd: string;
+}
+
+export interface UserOutDto {
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface UserDocument extends Document, UserOutDto {
+  _id: ObjectId;
+  pswd?: string;
+}
+
+const userSchema = new Schema<UserDocument>(
+  {
+    email: { type: String, required: true },
+    pswd: { type: String, required: true, select: false }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export const User = ReqisterTool<Model<UserDocument>>(USER_MODEL_NAME, () =>
+  model<UserDocument>(COLLECTION_NAME, userSchema)
+)();
